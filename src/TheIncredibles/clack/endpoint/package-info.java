0bec02dc6ae2a5883bd41e454aa7a07f5e7c4ec6
Solihah Adeboye <solihahadeboye.sia@gmail.com>
2024-09-16 16:53:59 -0400

    changed package name to TheIncredibles

---
+++ src/TheIncredibles/clack/endpoint/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes defining Clack's endpoints (Client and Server).
+ */
+package TheIncredibles.clack.endpoint;
---
+++ src/TheIncredibles/clack/message/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes defining the messages that Clack can send.
+ */
+package TheIncredibles.clack.message;
