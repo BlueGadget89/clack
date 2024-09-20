87694602ee308ca0dd83a3564aeb5170ac37cfad
Olaolu <olugbeoj@clarkson.edu>
2024-09-20 16:22:06 -0400

    initial changes to client. Merged with main.

--- src/TheIncredibles/clack/endpoint/Client.java
+++ src/TheIncredibles/clack/endpoint/Client.java
@@ -9,7 +9,7 @@ public class Client
      * a port listed as "unassigned" in
      * <a href="https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.txt">IANA</a>.
      */
-    public static final int DEFAULT_SERVER_PORT = 0 ; //TODO: choose an unassigned port (NOT 0!!)
+    public static final int DEFAULT_SERVER_PORT = 6; //TODO: choose an unassigned port (NOT 0!!)
 
     /**
      *  The server to connect to if one is not explicitly given.
@@ -23,24 +23,40 @@ public class Client
     private Message messageToSend;
     private Message messageReceived;
 
-    //TODO: JavaDoc
+    /**
+     *Client constructor
+     * @param username the name of the user of the Client
+     * @param serverName the name of the serve being used
+     * @param serverPort the portnumber to be connected to
+     */
     public Client(String username, String serverName, int serverPort) {
         this.username = username;
         this.serverName = serverName;
         this.serverPort = serverPort;
     }
 
-    //TODO: JavaDoc
+    /**
+     *Client constructor
+     * @param username the name of the user of the Client
+     * @param serverName the name of the serve being used
+     */
     public Client(String username, String serverName) {
         this(username, serverName, DEFAULT_SERVER_PORT);
     }
 
-    //TODO: JavaDoc
+    /**
+     *Client constructor
+     * @param username the name of the user of the Client
+     * @param serverPort the portnumber to be connected to
+     */
     public Client(String username, int serverPort) {
         this(username, DEFAULT_SERVER_NAME, serverPort);
     }
 
-    //TODO: JavaDoc
+    /**
+     *Client constructor
+     * @param username the name of the user of the Client
+     */
     public Client(String username) {
         this(username, DEFAULT_SERVER_NAME, DEFAULT_SERVER_PORT);
     }
@@ -74,19 +90,26 @@ public class Client
         //TODO: implement this.
     }
 
-    //TODO: JavaDoc
+    /**
+     * Gets the username of the user in use of the client
+     * @return A string representing the username of the user.
+     */
     public String getUsername() {
-        //TODO: implement this (return something other than null)
-        return null;
+        return username;
     }
 
-    //TODO: JavaDoc
+    /**
+     * Gets the name of the server being used
+     * @return A string representing the name of the server being used.
+     */
     public String getServerName() {
-        //TODO: implement this (return something other than null)
-        return null;
+        return serverName;
     }
 
-    //TODO: JavaDoc
+    /**
+     * Client parameters
+     * @return A string with the definitions of the various client parameters
+     */
     public String toString() {
         return "{class=Client|"
                 + "|DEFAULT_SERVER_NAME=" + DEFAULT_SERVER_NAME
@@ -94,7 +117,7 @@ public class Client
                 + "|username=" + this.username
                 + "|serverName=" + this.serverName
                 + "|serverPort=" + this.serverPort
-                + "|messageToSend={" + this.messageToSent.toString() + "}"
+                + "|messageToSend={" + this.messageToSend.toString() + "}"
                 + "|messageReceived={" + this.messageReceived.toString() + "}"
                 + "}";
     }
